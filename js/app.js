a87ac9bcae8156efdf596fae512a6d420ecb889d
charsdvp <carlossanchezdvp@gmail.com>
2023-07-18 13:35:10 -0600

    Change of color in the HTML structure, specifically the images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('hola');
+document.addEventListener('click', ( e ) => { 
+  const btnBurguer = document.querySelector('.container-btn-burguer') 
+  const $navbar = document.querySelector('.container-nav-list')
+  // Validamos los eventos click
+  if(btnBurguer.contains(e.target)) { $navbar.classList.toggle('isActive') }
+  if(!btnBurguer.contains(e.target)) { $navbar.classList.remove('isActive') }
+})
+
+window.addEventListener('scroll', ( e ) => {
+  const $navbar = document.querySelector('.container-nav')
+  // Validamos y aplicamos el efecto a la barra de navegacion
+  window.scrollY > 80 ? $navbar.style.background = '#222' : $navbar.style.background = '#111'  
+})
