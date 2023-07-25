ceec415583fd6cd3827eb5dc1ab68f27abab3875
charsdvp <carlossanchezdvp@gmail.com>
2023-07-25 11:09:28 -0600

    Contact section added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('click', ( e ) => { 
+document.addEventListener('click', ( e ) => {
   const btnBurguer = document.querySelector('.container-btn-burguer') 
   const $navbar = document.querySelector('.container-nav-list')
   // Validamos los eventos click
@@ -6,8 +6,9 @@ document.addEventListener('click', ( e ) => {
   if(!btnBurguer.contains(e.target)) { $navbar.classList.remove('isActive') }
 })
 
-window.addEventListener('scroll', ( e ) => {
-  const $navbar = document.querySelector('.container-nav')
-  // Validamos y aplicamos el efecto a la barra de navegacion
-  window.scrollY > 80 ? $navbar.style.background = '#222' : $navbar.style.background = '#111'  
-})
+// window.addEventListener('scroll', ( e ) => {
+//   const $navbar = document.querySelector('.container-nav')
+//   // Validamos y aplicamos el efecto a la barra de navegacion
+//   window.scrollY > 80 ? $navbar.style.background = '#222' : $navbar.style.background = '#111'
+//   console.log(scrollY)  
+// })
