09222a36ece24d412d664603764f17c200cd71b7
charsdvp <carlossanchezdvp@gmail.com>
2023-07-26 20:05:59 -0600

    fix: the error in the name for devices 320 has been corrected

--- js/app.js
+++ js/app.js
@@ -5,10 +5,3 @@ document.addEventListener('click', ( e ) => {
   if(btnBurguer.contains(e.target)) { $navbar.classList.toggle('isActive') }
   if(!btnBurguer.contains(e.target)) { $navbar.classList.remove('isActive') }
 })
-
-// window.addEventListener('scroll', ( e ) => {
-//   const $navbar = document.querySelector('.container-nav')
-//   // Validamos y aplicamos el efecto a la barra de navegacion
-//   window.scrollY > 80 ? $navbar.style.background = '#222' : $navbar.style.background = '#111'
-//   console.log(scrollY)  
-// })
